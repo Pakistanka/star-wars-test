50550bad9abe230221da804213b274ca14978f4e
Anastasia Polozova <polozova.anastasi@gmail.com>
2025-08-21 16:30:07 -0300

    config files

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/star-wars/',
+  resolve: {
+    alias: [
+      {
+        find: 'src',
+        replacement: resolve(__dirname, './src'),
+      },
+    ],
+  },
+});
